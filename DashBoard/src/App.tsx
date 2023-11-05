4457912393070208418310cdaf2b574c9d0890fc
Alex-Nav <alejugger@gmail.com>
2023-11-05 12:35:14 -0600

    Update App.tsx

--- DashBoard/src/App.tsx
+++ DashBoard/src/App.tsx
@@ -6,14 +6,22 @@ import './App.css'
 function cubeIt(num: number) {
   return (Math.cbrt(num))
 }
-function isLeap(year: number):boolean {
+function isLeap(year: number): boolean {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
   } else {
     return false;
   }
 }
-
+function onlyEvens(digits: number[]):number[] {
+  let evens: number[] = []
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] % 2 === 0) {
+      evens.push(digits[i])
+    }
+  }
+  return evens
+}
 function App() {
   return (
     <>
@@ -27,7 +35,8 @@ function App() {
       </div>
       <h1>DashBoard 34</h1>
       <p>{cubeIt(127)}</p>
-      {isLeap(100)? <p>YYES</p>:<p>NOO</p>}
+      {isLeap(100) ? <p>YYES</p> : <p>NOO</p>}
+      <p>onlyEvens: {onlyEvens([3, 4, 5, 6, 7, 8, 9, 10,11,33,36])}</p>
     </>
   )
 }
